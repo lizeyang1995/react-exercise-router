6b23b8238c24bc88b09d5b5520d57f716c7a2402
lizeyang1995 <424011527@qq.com>
2020-09-23 20:02:13 +0800

    [lizeyang] faet: create Home.js

---
+++ src/exercise-1/components/home/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <p>
+          This is beautiful Home Page.
+        </p>
+        <p>
+          And the url is '/'.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
+
